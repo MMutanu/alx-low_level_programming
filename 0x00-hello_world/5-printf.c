11b09d70aedecdbda44afcae27778c8dbf577bb9
MMutanu <melody.mutanu@gmail.com>
2022-10-14 15:25:45 -0700

    updating the 5-printf.c code

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char Mystr[] = "\"Programming is like building a multilingual puzzle";
+
+	printf(Mystr);
+
+	return (0);
+
+}

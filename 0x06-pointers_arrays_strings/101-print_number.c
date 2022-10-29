8bd695ee199141dbe4926af6e4b01eddeb07b6c6
MMutanu <melody.mutanu@gmail.com>
2022-10-29 14:46:45 -0700

    initial commit

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,31 @@
+/**
+ * digits - Numbers digits of a number
+ * @n: Number
+ * Return: Digits
+ */
+unsigned int digits(unsigned int n)
+{
+	int i;
+
+	i = 0;
+	while (n != 0)
+	{
+		i++;
+		n /= 10;
+	}
+
+	return (i);
+}
+
+unsigned int pow_x10(unsigned int exponent)
+{
+	unsigned int res, i;
+
+	res = 1;
+	for (i = 1; i < exponent; i++)
+	{
+		res *= 10;
+	}
+
+	return (res);
+}

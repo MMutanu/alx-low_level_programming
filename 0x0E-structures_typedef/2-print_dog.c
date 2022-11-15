95e5034fe0e0454ecceb31d3064e03d852096f76
MMutanu <melody.mutanu@gmail.com>
2022-11-15 12:55:45 -0800

    initial commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,18 +8,27 @@
  **/
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
-		;
+	if (!d)
+	{
+		return;
+	}
+	if (d->name)
+	{
+		printf("Name: %s\n", d->name);
+	}
 	else
 	{
-		if (d->name != NULL)
-			printf("Name: %s\n", d->name);
-		else
-			printf("Name: (nil)\n");
-		printf("Age: %f\n", d->age);
-		if (d->owner != NULL)
-			printf("Owner: %s\n", d->owner);
-		else
-			printf("Owner: (nil)\n");
+		printf("Name: (nil)\n");
+	}
+
+	printf("Age: %f\n", d->age);
+
+	if (d->owner)
+	{
+		printf("Owner: %s\n", d->owner);
+	}
+	else
+	{
+		printf("Owner: (nil)\n");
 	}
 }

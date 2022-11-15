3ae4544f637e8eadb78d45ac48f50733a8ac1402
MMutanu <melody.mutanu@gmail.com>
2022-11-15 11:48:59 -0800

    initial commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+ifndef DOG_H
+define DOG_H
+/*
+*struct dog: structure listing dog details
+*Description: Define a new type struct dog
+*name- dog's name
+*owner-owner of the dog
+*age-dog's age
+*/
+struct dog
+{
+char *name;
+char *owner;
+float *age;
+};
+
+int main(void)
+{
+struct dog mydog;
+}

ca6a60cceafd9d093949e3c637d74592929cc844
MMutanu <melody.mutanu@gmail.com>
2022-11-15 13:34:41 -0800

    initial commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,7 +12,7 @@ struct dog
 	char *owner;
 	float age;
 };
-struct dog dog_t;
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 

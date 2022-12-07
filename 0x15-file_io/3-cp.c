8f3ecb87860e50d283a9a07c926a3e1e16c9932e
MMutanu <melody.mutanu@gmail.com>
2022-12-07 12:06:52 -0800

    initial commit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -49,22 +49,3 @@ int main(int argc, char *argv[])
 
 	/*sets file descriptor for copy-from file*/
 	fd_1 = open(argv[1], O_RDONLY);
-	if (fd_1 == -1)
-		__exit(98, argv[1], 0);
-
-	/*reads original file as long as there's more than 0 to read*/
-	/*copies/writes contents into new file */
-	while ((n_read = read(fd_1, buffer, 1024)) != 0)
-	{
-		if (n_read == -1)
-			__exit(98, argv[1], 0);
-
-		n_wrote = write(fd_2, buffer, n_read);
-		if (n_wrote == -1)
-			__exit(99, argv[2], 0);
-	}
-
-	close(fd_2) == -1 ? (__exit(100, NULL, fd_2)) : close(fd_2);
-	close(fd_1) == -1 ? (__exit(100, NULL, fd_1)) : close(fd_1);
-	return (0);
-}
